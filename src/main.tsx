455376936ebd9930d29094249fce26fe16ae1c83
AlphaDureku <templanzamark2002@gmail.com>
2023-10-30 16:45:13 +0800

    moved css files to asset

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./HomePageStyling.css";
-import "./ReusableComponentsStyling.css";
+import "./assets/HomePageStyling.css";
+import "./assets/ReusableComponentsStyling.css";
 
 import "./index.css";
 
