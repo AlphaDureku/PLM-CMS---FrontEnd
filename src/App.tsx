d13498ff29b3a1b7e4bbf2d2b65a21c9d13ebe48
AlphaDureku <templanzamark2002@gmail.com>
2023-11-09 19:03:18 +0800

    added TestSite page for testing components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "@mantine/core/styles.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import LogIn from "./components/pages/LogIn";
+import TestSite from "./components/pages/TestSite";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<LogIn />} />
+          <Route path="/TestSite" element={<TestSite />} />
         </Routes>
       </BrowserRouter>
     </>
