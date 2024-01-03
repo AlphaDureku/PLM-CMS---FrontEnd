f69f49e9b76b6b19b6c2517f3198c4301ebefb6b
AlphaDureku <templanzamark2002@gmail.com>
2024-01-04 04:03:53 +0800

    fixed a typescript error

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
+import { MantineProvider, createTheme } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/HomePageStyling.css";
 import "./assets/LogInPageStyling.css";
 import "./assets/ReusableComponentsStyling.css";
-
 import "./index.css";
 
-import { MantineProvider, createTheme } from "@mantine/core";
+const theme = createTheme({});
 
-const theme = createTheme({
+const rootElement = document.getElementById("root");
 
 
-});
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(rootElement!).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
 );
+} 
