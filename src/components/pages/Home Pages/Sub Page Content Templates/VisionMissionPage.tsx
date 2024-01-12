d8e7dcdf227959520ad7687092c5f01159a2ab31
AlphaDureku <templanzamark2002@gmail.com>
2024-01-12 20:23:21 +0800

    up

---
+++ src/components/pages/Home Pages/Sub Page Content Templates/VisionMissionPage.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "@mantine/core";
+
+export default function VisionMissionPage() {
+
+
+    return (
+        <Flex>
+            <div>
+                <h3>Vision</h3>
+            </div>
+            <div>
+                <h3>Mission</h3>
+            </div>
+            <div>
+                <h3>Core Values</h3>
+            </div>
+        </Flex>)
+}
